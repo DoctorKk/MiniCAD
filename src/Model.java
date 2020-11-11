d91c1c9d906c18cb1e910bc4aed211363fb04b71
DoctorKk <917665967@qq.com>
2020-11-11 17:13:29 +0800

    加了些Button

--- src/Model.java
+++ src/Model.java
@@ -4,48 +4,57 @@ import javax.swing.*;
 import java.awt.*;
 //import javax.swing.JPanel;
 
-public class Model extends JPanel{
-    
+public class Model extends JPanel {
+    public static int CurrentState = 0; // 0选中，1画线，2画圆,3文本
+
+    Model() {
+
+    }
+
 }
 
-
-class Shape extends JPanel{
+class Shape extends JPanel {
     private int color;
     private int size;
+    private boolean chosen;
 
-    Shape(){}
+    Shape() {
+    }
 
-    void render(Graphics g){}
+    void render(Graphics g) {
+    }
 
-    
 }
 
-class Line extends Shape{
+class Line extends Shape {
     private int xStart, yStart;
     private int xEnd, yEnd;
 
-    Line(){}
+    Line() {
+    }
 
-    Line setXStart(int x){
+    Line setXStart(int x) {
         this.xStart = x;
         return this;
     }
-    Line setYStart(int y){
+
+    Line setYStart(int y) {
         this.yStart = y;
         return this;
     }
-    Line setXEnd(int x){
+
+    Line setXEnd(int x) {
         this.xEnd = x;
         return this;
     }
-    Line setYEnd(int y){
+
+    Line setYEnd(int y) {
         this.yEnd = y;
         return this;
     }
 
-    
     @Override
-    void render(Graphics g){
+    void render(Graphics g) {
 
     }
 }
--- src/View.java
+++ src/View.java
@@ -1,7 +1,121 @@
 package src;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import src.Model;
 
-public class View extends JPanel{
-    
+public class View extends JPanel {
+    View() {
+
+    }
+}
+
+class MyFrame extends JFrame {
+    private final String TITLE = "MiniCAD";
+    private final int HEIGHT = 1080;
+    private final int WIDTH = 960;
+
+    MyFrame() {
+        super();
+        init();
+    }
+
+    private void init() {
+        setTitle(TITLE);
+        setSize(WIDTH, HEIGHT);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+
+        MyPanel panel = new MyPanel(this);
+        setContentPane(panel);
+    }
+}
+
+class MyPanel extends JPanel {
+    private MyFrame frame;
+
+    MyPanel(MyFrame frame) {
+        super(new BorderLayout());
+        this.frame = frame;
+    }
+
+    private void init() {
+        Frame Left = new Frame();
+
+        add(Left, BorderLayout.EAST);
+    }
+
+}
+
+class Frame extends JPanel {
+    Frame() {
+        super();
+    }
+
+    private void init() {
+        final JButton btnClick = new JButton("CLICK");
+        btnClick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Model.CurrentState = 0;
+            }
+        });
+
+        final JButton btnDrawLine = new JButton("Draw Lines");
+        btnDrawLine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Model.CurrentState = 1;
+            }
+        });
+
+        final JButton btnDrawCircle = new JButton("Draw Circles");
+        btnDrawCircle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Model.CurrentState = 2;
+            }
+        });
+
+        final JButton btnText = new JButton("TEXT");
+        btnText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Model.CurrentState = 3;
+            }
+        });
+
+        final JButton btnSave = new JButton("Save");
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Model.CurrentState = 3;
+            }
+        });
+
+        final JButton btnLoad = new JButton("Load");
+        btnLoad.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Model.CurrentState = 3;
+            }
+        });
+
+        Box vBox = Box.createHorizontalBox();
+        vBox.add(btnClick);
+        vBox.add(btnDrawLine);
+        vBox.add(btnDrawCircle);
+        vBox.add(btnText);
+        vBox.add(btnSave);
+        vBox.add(btnLoad);
+        add(vBox);
+    }
+
+}
+
+class DrawBoard extends JPanel {
+
 }
